84bedeac91466b78c811c8333a354c7da476feaa
Beatryz Couto <61602714+coutobeatryz@users.noreply.github.com>
2024-01-27 11:18:10 -0300

    Create Aluno.java

---
+++ Classes/Programação de Computadores II/Práticas/Prática 2/Aluno.java
@@ -0,0 +1,13 @@
+package ufop;
+
+public class Aluno {
+    public String nome;
+    public String matricula;
+    public int idade;
+
+    public Aluno(String nome, String matricula, int idade) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.idade = idade;
+    }
+}
