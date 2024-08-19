a3b737f84847c4a893686ccbde15e25248f1306c
Beatryz Couto <61602714+coutobeatryz@users.noreply.github.com>
2024-08-19 17:32:50 -0300

    Update Exercício TAD Lista 1.c

--- Classes/AEDS I/24.1/TAD/Exercício TAD Lista 1.c
+++ Classes/AEDS I/24.1/TAD/Exercício TAD Lista 1.c
@@ -1,146 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+ #include <stdlib.h>
 
-typedef struct {
-    int codigo;
-    char nome[100]
-} TProduto;
-
-typedef struct celula {
-    TProduto item;
-    struct celula* prox;
-} TCelula;
-
-typedef struct {
-    TCelula* primeiro;
-    TCelula* ultimo;
-    int tamanho;
-} TLista;
-
-void FLVazia(TLista *Lista){
-    Lista -> primeiro = (TCelula *) malloc(sizeof(TCelula));
-    Lista -> ultimo = Lista -> primeiro;
-    Lista -> primeiro -> prox = NULL;
-    Lista -> tamanho = 0;
-}
-
-void Inserir(TProduto x, TLista *Lista){
-    Lista -> ultimo -> prox = (TCelula *) malloc(sizeof(TCelula));
-    Lista -> ultimo = Lista -> ultimo -> prox;
-    Lista -> ultimo -> item = x;
-    Lista -> ultimo -> prox = NULL;
-    Lista -> tamanho++;
-}
-
-void Imprimir(TLista Lista){
-    TCelula* Aux;
-    Aux = Lista.primeiro -> prox;
-    while (Aux != NULL){
-        printf("Codigo: %d\n Nome: %s\n", Aux -> item.codigo, Aux -> item.nome);
-        Aux = Aux -> prox;
-    }
-}
-
-TCelula* Pesquisar(TLista Lista, TProduto Item){
-    TCelula* Aux;
-    Aux = Lista.primeiro;
-    while(Aux->prox != NULL){
-        if(Aux->prox->item.codigo == Item.codigo)
-        return Aux;
-        Aux = Aux -> prox;
-    }
-    return NULL;
-}
-
-void Excluir(TLista *Lista, TProduto *Item){
-    TCelula *Aux1, *Aux2;
-    Aux1 = Pesquisar(Lista, *Item);
-    if (Aux1 != NULL){
-        Aux2 = Aux1 -> prox;
-        Aux1 -> prox = Aux2 -> prox;
-        *Item = Aux2 -> item;
-        if(Aux1 -> prox == NULL)
-        Lista->ultimo = Aux1;
-        free(Aux2);
-        Lista->tamanho--;
-    }
-}
-
-void LerProduto(TProduto *produto){
-    fflush(stdin);
-    printf("Digite o código do produto: ");
-    scanf("%d", &produto->codigo);
-    fflush(stdin);
-    printf("Digite o nome do produto: ");
-    fgets(produto->nome, 100, stdin);
-    produto->nome[strcspn(produto->nome, "\n")] = 0;
-}
-
-void ImprimirProduto(TProduto produto){
-    printf("Codigo do produto: %d\n", produto.codigo);
-    printf("Nome do produto: %s\n", produto.nome);
-}
-
-TCelula* PesquisarPorNome(TLista Lista, char* nome){
-    TCelula* Aux = Lista.primeiro->prox;
-    while (Aux != NULL){
-        if (strcmp(Aux->item.nome, nome) == 0){
-            return Aux;
-        }
-        Aux = Aux->prox;
-    }
-    return NULL;
-}
+  typedef struct {
+    int code;
+    char name[50];
+    int quantity;
+    float price;
+  } Product;
 
 int main() {
-    TLista Lista1, Lista2;
-    TProduto Item;
+    int products_num;
 
-    FLVazia(&Lista1);
-    FLVazia(&Lista2);
+    printf("Enter the products number to be registrated: ");
+    scanf("%d", &products_num);
 
-    for(int i=0; i < 10; i++){
-    LerProduto(&Item);
-    Inserir(Item, &Lista1);
-    Inserir(Item, &Lista2);
-}
+    Product* products = (Product*)malloc(products_num * sizeof(Product));
+    if (products == NULL) {
+        printf("Alocation Memory Error!");
+        return 1;
+    }
 
-printf("Lista 1:\n");
-Imprimir(Lista1);
-printf("Lista 2:\n");
-Imprimir(Lista2);
+    for (int i = 0; i < products_num; i++){
+        printf("\nProduct registration: \n", i + 1);
+        printf("Code: ");
+        scanf("%d", &products[i].code);
+        printf("Name: ");
+        scanf("%s", products[i].name);
+        printf("Available quantity: ");
+        scanf("%d", &products[i].quantity);
+        printf("Sale price");
+        scanf("%f", &products[i].price);
+    }
 
-Item = Lista1.primeiro->prox->prox->prox->item;
-Excluir(&Lista1, &Item);
+    int idx_biggest_price = 0;
+    
+    for (int i = 1; i < products_num; i++){
+        if(products[i].price > products[idx_biggest_price].price){
+            idx_biggest_price = i;
+        }
+    }
 
-Item = Lista1.primeiro->prox->item;
-Excluir(&Lista1, &Item);
+    int idx_biggest_quantity = 0;
+    for(int i = 1; i < products_num; i++){
+        if(products[i].price > products[idx_biggest_quantity].quantity){
+            idx_biggest_quantity = i;
+        }
+    }
 
-Item = Lista1.primeiro->prox->prox->item;
-Excluir(&Lista1, &Item);
+    printf("\nProduct with highest selling price: %s\n", products[idx_biggest_price].name);
+    printf("Price: %f", products[idx_biggest_price].price);
+    printf("Product with the largest quantity available in stock: %s", products[idx_biggest_quantity].name);
+    printf("Quantity: %s", products[idx_biggest_quantity].quantity);
 
-Item = Lista1.primeiro->prox->item;
-Excluir(&Lista1, &Item);
+    free(products);
 
-printf("Lista 1 após exclusões:\n");
-Imprimir(Lista1);
-printf("Lista 2 permanece inalterada:\n");
-Imprimir(Lista2);
-
-char nomePesquisa[100];
-fflush(stdin);
-printf("Digite o nome do produto para pesquisar: ");
-fgets(nomePesquisa, 100, stdin);
-nomePesquisa[strcspn(nomePesquisa, "\n")] = 0;
-
-TCelula* encontrado = PesquisarPorNome(Lista1, nomePesquisa);
-if (encontrado != NULL) {
-    printf("Produto encontrado:\n");
-    ImprimirProduto(encontrado->item);
-} else {
-    printf("Produto não encontrado.\n");
-}
-
-return 0;
+    return 0;
 }
